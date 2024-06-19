7abb44f6fbf48dd34682fb4e9f766e6bbfacf38f
stovecbr <pc-018@st-ove.co.jp>
2024-06-19 15:54:07 +0900

    説明用のコメントが多いと見辛いので減らした #6

--- scraper.py
+++ scraper.py
@@ -14,20 +14,17 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-# Get the directory of the current script or executable
+# 現在のディレクトリのパスを取得
 current_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
 
-# Construct the path to the chromedriver
+# 環境変数RUNNING_IN_DOCKERが設定されている場合は、Docker環境で実行されていることを示します。
 if os.environ.get('RUNNING_IN_DOCKER'):
     chromedriver_path = '/usr/bin/chromedriver'
 else:
     chromedriver_path = os.path.join(current_dir, 'chromedriver.exe')
 
-
-
-# ChromeDriverのパスを設定
-# webdriver_service = Service('/usr/bin/chromedriver')
-
+# WebDriverのサービスを設定
+# サービスを設定することで、WebDriverを実行するためのバックグラウンドプロセスが作成されます。
 webdriver_service = Service(chromedriver_path)
 
 # ディレクトリのパス
@@ -41,7 +38,6 @@ if not os.path.exists(dir_path):
 default_chrome_paths = {
     'Linux': '/usr/bin/google-chrome-stable',
     'Windows': 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-    # 他のシステムのデフォルトのパスを追加することも可能です
 }
 
 # 現在のシステムの名前を取得
@@ -52,18 +48,8 @@ chrome_options = Options()
 chrome_options.binary_location = os.getenv('CHROME_PATH', default_chrome_paths.get(system_name, '/usr/bin/google-chrome-stable'))
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
-# デバッグ用のシャーレを無効にします。
-# これにより、一部の環境で問題を引き起こすことがあるメモリの使用量を削減できます。
-# Docker環境では/dev/shmのサイズがデフォルトで制限されており、この制限によりChromeがクラッシュすることがあります。
-# chrome_options.add_argument("--disable-dev-shm-usage")
-
-# Chromeのバイナリのパスを指定します。
-# chrome_options.binary_location = "/usr/bin/google-chrome-stable"
-
-chrome_options.add_argument("--disable-gpu")  # GPUを無効にする
-
-chrome_options.add_argument("--remote-debugging-port=9222")  # リモートデバッグポートを設定する
-
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--remote-debugging-port=9222") 
 
 # WebDriverの設定
 driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
@@ -72,12 +58,11 @@ driver.get('https://www.coconala.com/categories')
 
 # データ収集の例
 titles = []
-# 1から10までの範囲でループします。
-for i in range(1, 21):  # 上から20個のアイテムを収集
+# 1から10までの範囲でループ
+for i in range(1, 11):  
 # try-except文を使用して、例外が発生した場合にエラーメッセージを表示します。
     try:
         # find_elementメソッドを使用して、指定された要素を取得します。
-        # これにより、指定されたXPathパターンに一致する要素が取得されます。
         title = driver.find_element(By.XPATH, f'//*[@id="__layout"]/div/div[2]/div[1]/div/ul/li[1]/ul/li[{i}]/a').text
         # appendメソッドを使用して、リストに要素を追加します。
         titles.append(title)
