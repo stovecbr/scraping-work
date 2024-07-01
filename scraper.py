f9944f851d8983abf26f20ec23c02a6025d9729d
stovecbr <pc-018@st-ove.co.jp>
2024-07-01 10:23:38 +0900

    Pylint をインストールした後にでたエラーを修正した #11

--- scraper.py
+++ scraper.py
@@ -1,3 +1,12 @@
+"""
+このファイルは、Coconalaのカテゴリタイトルを取得するスクレイピングプログラムです。
+このプログラムは、Seleniumを使用してWebページからデータを取得します。
+Seleniumは、Webブラウザを自動化するためのツールです。
+このプログラムは、Google ChromeのWebDriverを使用してWebページからデータを取得します。
+WebDriverは、Webブラウザを制御するためのAPIです。
+このプログラムは、Google ChromeのWebDriverを使用してWebページからデータを取得します。
+"""
+
 import sys
 import os
 import platform
@@ -7,37 +16,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support import expected_conditions
+# from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import (
+    NoSuchElementException,
+)  # NoSuchElementExceptionをインポート
 
 
 # 現在のディレクトリのパスを取得
-current_dir = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+current_dir = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
 
 # 環境変数RUNNING_IN_DOCKERが設定されている場合は、Docker環境で実行されていることを示します。
-if os.environ.get('RUNNING_IN_DOCKER'):
-    chromedriver_path = '/usr/bin/chromedriver'
+if os.environ.get("RUNNING_IN_DOCKER"):
+    CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
 else:
-    chromedriver_path = os.path.join(current_dir, 'chromedriver.exe')
+    CHROMEDRIVER_PATH = os.path.join(current_dir, "chromedriver.exe")
 
 # WebDriverのサービスを設定
 # サービスを設定することで、WebDriverを実行するためのバックグラウンドプロセスが作成されます。
-webdriver_service = Service(chromedriver_path)
+webdriver_service = Service(CHROMEDRIVER_PATH)
 
 # ディレクトリのパス
-dir_path = 'data'
+DIR_PATH = "data"
 
 # ディレクトリが存在しない場合は作成する
-if not os.path.exists(dir_path):
-    os.makedirs(dir_path)
+if not os.path.exists(DIR_PATH):
+    os.makedirs(DIR_PATH)
 
 # システムごとのデフォルトのChromeのバイナリのパス
 default_chrome_paths = {
-    'Linux': '/usr/bin/google-chrome-stable',
-    'Windows': 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    "Linux": "/usr/bin/google-chrome-stable",
+    "Windows": "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
 }
 
 # 現在のシステムの名前を取得
@@ -45,29 +58,34 @@ system_name = platform.system()
 
 # Chromeの設定
 chrome_options = Options()
-chrome_options.binary_location = os.getenv('CHROME_PATH', default_chrome_paths.get(system_name, '/usr/bin/google-chrome-stable'))
+chrome_options.binary_location = os.getenv(
+    "CHROME_PATH",
+    default_chrome_paths.get(system_name, "/usr/bin/google-chrome-stable"),
+)
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--remote-debugging-port=9222") 
+chrome_options.add_argument("--remote-debugging-port=9222")
 
 # WebDriverの設定
 driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+# driver = webdriver.Chrome()
 # ココナラのページにアクセス
-driver.get('https://www.coconala.com/categories')
+driver.get("https://www.coconala.com/categories")
 
 # データ収集の例
 titles = []
 # 1から10までの範囲でループ
-for i in range(1, 11):  
-# try-except文を使用して、例外が発生した場合にエラーメッセージを表示します。
-    try:
-        # find_elementメソッドを使用して、指定された要素を取得します。
-        title = driver.find_element(By.XPATH, f'//*[@id="__layout"]/div/div[2]/div[1]/div/ul/li[1]/ul/li[{i}]/a').text
-        # appendメソッドを使用して、リストに要素を追加します。
-        titles.append(title)
+for i in range(
+    1, 11
+):  # try-except文を使用して、例外が発生した場合にエラーメッセージを表示します。
+    try:  # find_elementメソッドを使用して、指定された要素を取得します。
+        title = driver.find_element(
+            By.XPATH, f'//*[@id="__layout"]/div/div[2]/div[1]/div/ul/li[1]/ul/li[{i}]/a'
+        ).text
+        titles.append(title)  # appendメソッドを使用して、リストに要素を追加します。
         # exceptブロックを使用して、例外が発生した場合にエラーメッセージを表示します。
-    except Exception as e:
+    except NoSuchElementException as e:  # NoSuchElementExceptionをキャッチ
         # print関数を使用して、エラーメッセージを表示します。
         print(f"Error: {e}")
         # timeモジュールのsleep関数を使用して、プログラムの実行を一時停止します。
@@ -76,7 +94,7 @@ for i in range(1, 11):
 driver.quit()
 
 # データフレームに整形
-df = pd.DataFrame(titles, columns=['🐼category title🐼'])
+df = pd.DataFrame(titles, columns=["🐼category title🐼"])
 
 # データをCSVファイルとして保存
-df.to_csv('./data/coconala_titles.csv', index=False, encoding='utf-8')
+df.to_csv("./data/coconala_titles.csv", index=False, encoding="utf-8")
